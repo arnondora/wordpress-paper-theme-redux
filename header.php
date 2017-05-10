c730c23e333dcc07d1b85fa59221e8a8b87dcd81
Arnon Puitrakul <peter_arnon1@icloud.com>
2017-05-11 02:56:49 +0700

    Init Commit For Pages File

---
+++ header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <!-- <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/dist/css/PaperTheme.css" type="text/css" media="all" /> -->
+    <title>PaperTheme 3.0</title>
+    <?php wp_head() ?>
+  </head>
+  <body>
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php get_header()?>
+
+<!-- Content Goes Here -->
+<h1 class = "scale-transition">This is the test from papertheme 3.0</h1>
+
+<?php get_footer()?>
