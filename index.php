1506f0d77f6287eb40527b2fe3524bfd6d43758b
Arnon Puitrakul <peter_arnon1@icloud.com>
2017-05-11 23:36:39 +0700

    Implement Search Box

--- index.php
+++ index.php
@@ -3,6 +3,19 @@
 <main>
   <!-- Content Goes Here -->
   <div class = "container">
+
+    <!-- Search Box  -->
+    <div class = "row">
+      <div class = "searchbox-outer">
+        <form method = "post">
+          <div class="input-field">
+            <i class = "fa fa-search search-font-color prefix" style = "margin-top:8px"></i>
+            <input type = "text" placeholder="Search something new!">
+          </div>
+        </form>
+      </div>
+    </div>
+
     <h1 class = "scale-transition">Hello PaperTheme 3.0 !</h1>
     <blockquote>
       Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
