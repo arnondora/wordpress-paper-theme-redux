9cd4be520c36fe70a0117f59cb7d50d74b239248
Arnon Puitrakul <peter_arnon1@icloud.com>
2017-05-12 17:11:57 +0700

    Generate The Loop Example

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php get_header()?>
 
-<main class = "index">
+<main <?php body_class()?>>
   <!-- Content Goes Here -->
   <div class = "container">
 
@@ -16,10 +16,22 @@
       </div>
     </div>
 
+    <?php if (is_home()):?>
     <h1 class = "scale-transition">Hello PaperTheme 3.0 !</h1>
     <blockquote>
       Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
     </blockquote>
+    <?php endif?>
+
+    <?php
+      if(have_posts() ):
+        while(have_posts() ): the_post();?>
+          <h3><?php the_title();?></h3>
+          <small>Posted on <?php the_time('F j, Y') ?> | <?php the_category()?></small>
+          </p><?php the_content();?></p>
+          <hr>
+        <?php endwhile;
+      endif;?>
   </div>
 </main>
 
