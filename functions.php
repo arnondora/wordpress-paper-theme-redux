693b505b18d7e95a0239adc093b2f2105c80fb47
Arnon Puitrakul <peter_arnon1@icloud.com>
2017-05-16 02:16:43 +0700

    Add Theme HTML5 Support on comment-list and comment-form

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@ function papertheme_theme_setup()
   //Hook Theme Support List
   add_theme_support('menus');
   add_theme_support('post-thumbnails');
-  add_theme_support('html5', array('search-form'));
+  add_theme_support('html5', array('search-form', 'comment-list', 'comment-form'));
 
   //Register Nav Menu
   register_nav_menu('primary', 'Sidebar Navigation');
