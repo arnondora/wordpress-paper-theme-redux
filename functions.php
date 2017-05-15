2b79ba838cbb75fe0eb14e93522ec719ec7d61d1
Arnon Puitrakul <peter_arnon1@icloud.com>
2017-05-15 21:31:21 +0700

    Enable HTML5 functionality on search box

--- functions.php
+++ functions.php
@@ -6,6 +6,7 @@ function papertheme_theme_setup()
   //Hook Theme Support List
   add_theme_support('menus');
   add_theme_support('post-thumbnails');
+  add_theme_support('html5', array('search-form'));
 
   //Register Nav Menu
   register_nav_menu('primary', 'Sidebar Navigation');
