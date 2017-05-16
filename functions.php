9b10d4f43d41540dc7e8e798bd22b16ace2095f5
Arnon Puitrakul <peter_arnon1@icloud.com>
2017-05-16 15:34:00 +0700

    Enable AMP

--- functions.php
+++ functions.php
@@ -20,7 +20,9 @@ function papertheme_enquque_assets()
   wp_enqueue_style('papertheme_style');
 
   wp_register_script('papertheme_script', get_template_directory_uri() . '/dist/js/PaperTheme.js', array(),'1.0', true);
+  wp_register_script('papertheme_amp', 'https://cdn.ampproject.org/v0.js', array(),'1.0', true);
   wp_enqueue_script('papertheme_script');
+  wp_enqueue_script('papertheme_amp');
 }
 
 add_action( 'wp_enqueue_scripts', 'papertheme_enquque_assets' );
