ea65c24bf93c280e19213e818d209d5bae24065b
Arnon Puitrakul <peter_arnon1@icloud.com>
2017-06-07 23:54:53 +0700

    Install Redux Framework

--- functions.php
+++ functions.php
@@ -74,4 +74,11 @@ class papertheme_primary_menu_nav_walker extends Walker_Nav_Menu
             }
 }
 
+if ( !class_exists( 'ReduxFramework' ) && file_exists( dirname( __FILE__ ) . '/ReduxFramework/ReduxCore/framework.php' ) ) {
+    require_once( dirname( __FILE__ ) . '/ReduxFramework/ReduxCore/framework.php' );
+}
+if ( !isset( $redux_demo ) && file_exists( dirname( __FILE__ ) . '/ReduxFramework/sample/sample-config.php' ) ) {
+    require_once( dirname( __FILE__ ) . '/ReduxFramework/sample/sample-config.php' );
+}
+
 ?>
