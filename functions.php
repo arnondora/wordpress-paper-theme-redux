c4c126dec828b03874b6a1c392c7ecdcfb427b5c
Arnon Puitrakul <peter_arnon1@icloud.com>
2017-06-09 00:13:44 +0700

    Swap css loading order

--- functions.php
+++ functions.php
@@ -18,8 +18,8 @@ function papertheme_enquque_assets()
 {
   wp_register_style('papertheme_style', get_template_directory_uri() . '/dist/css/PaperTheme.css', array(),'1.1.0', all);
   wp_register_style('font-awesome', get_template_directory_uri() . '/dist/css/font-awesome.min.css', array(),'1.0', all);
-  wp_enqueue_style('font-awesome');
   wp_enqueue_style('papertheme_style');
+  wp_enqueue_style('font-awesome');
 
   wp_register_script('papertheme_script', get_template_directory_uri() . '/dist/js/PaperTheme.js', array(),'1.1.0', true);
   wp_register_script('papertheme_amp', 'https://cdn.ampproject.org/v0.js', array(),'1.0', true);
