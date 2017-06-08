678df2141a2e21bb73a6cba8cdd402dd97167e8e
Arnon Puitrakul <peter_arnon1@icloud.com>
2017-06-08 12:49:46 +0700

    Update Version Number in package.json and stylesheet version

--- functions.php
+++ functions.php
@@ -16,12 +16,12 @@ add_action('init', 'papertheme_theme_setup');
 //Function for enqueue all assets (including related stylesheet and javascript)
 function papertheme_enquque_assets()
 {
-  wp_register_style('papertheme_style', get_template_directory_uri() . '/dist/css/PaperTheme.css', array(),'1.0', all);
+  wp_register_style('papertheme_style', get_template_directory_uri() . '/dist/css/PaperTheme.css', array(),'1.1.0', all);
   wp_register_style('font-awesome', get_template_directory_uri() . '/dist/css/font-awesome.min.css', array(),'1.0', all);
   wp_enqueue_style('font-awesome');
   wp_enqueue_style('papertheme_style');
 
-  wp_register_script('papertheme_script', get_template_directory_uri() . '/dist/js/PaperTheme.js', array(),'1.0', true);
+  wp_register_script('papertheme_script', get_template_directory_uri() . '/dist/js/PaperTheme.js', array(),'1.1.0', true);
   wp_register_script('papertheme_amp', 'https://cdn.ampproject.org/v0.js', array(),'1.0', true);
   wp_enqueue_script('papertheme_script');
   wp_enqueue_script('papertheme_amp');
