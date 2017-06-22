c2e1d8209d452b3ee3509ad3a483a6b71a333c98
Arnon Puitrakul <peter_arnon1@icloud.com>
2017-06-23 00:11:37 +0700

    Add Label to search box

--- searchform.php
+++ searchform.php
@@ -2,7 +2,7 @@
   <form role = "search" method = "get" action =  "<?php echo home_url('/')?>">
     <div class="input-field">
       <i class = "fa fa-search search-font-color prefix" style = "margin-top:8px"></i>
-      <input type = "text" name = "s" placeholder="Search something new!">
+      <input type = "text" aria-label="search-box" name = "s" placeholder="Search something new!">
     </div>
   </form>
 </div>
