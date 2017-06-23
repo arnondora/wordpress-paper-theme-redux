fa6593e0de2f4f9abab87bcc288d8ac2986ebe95
Arnon Puitrakul <peter_arnon1@icloud.com>
2017-06-23 11:10:11 +0700

    Add title-tag theme support

--- functions.php
+++ functions.php
@@ -8,6 +8,7 @@ function papertheme_theme_setup()
   add_theme_support('post-thumbnails');
   add_theme_support('html5', array('search-form', 'comment-list', 'comment-form'));
   add_theme_support( 'automatic-feed-links' );
+  add_theme_support('title-tag')
 
   //Register Nav Menu
   register_nav_menu('primary', 'Sidebar Navigation');
