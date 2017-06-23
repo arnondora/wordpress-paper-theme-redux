e8640b1b8ce6c3d9b7fe661b3c9275b8922ca48e
Arnon Puitrakul <peter_arnon1@icloud.com>
2017-06-23 10:47:01 +0700

    Change language attribute in html to use wordpress function instead

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html amp lang="th">
+<html amp <?php language_attributes(); ?>>
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
