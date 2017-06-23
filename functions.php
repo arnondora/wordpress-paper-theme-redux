76b8c23c3b5a06add8f98ef575ab0a5732c44d9d
Arnon Puitrakul <peter_arnon1@icloud.com>
2017-06-23 10:51:47 +0700

    add automatic-feed-links support

--- functions.php
+++ functions.php
@@ -7,6 +7,7 @@ function papertheme_theme_setup()
   add_theme_support('menus');
   add_theme_support('post-thumbnails');
   add_theme_support('html5', array('search-form', 'comment-list', 'comment-form'));
+  add_theme_support( 'automatic-feed-links' );
 
   //Register Nav Menu
   register_nav_menu('primary', 'Sidebar Navigation');
