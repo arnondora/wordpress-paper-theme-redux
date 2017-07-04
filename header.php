3b902241e8d30447ba7ea199b9c84fd9debf1faf
Arnon Puitrakul <peter_arnon1@icloud.com>
2017-07-05 00:13:21 +0700

    Remove Repeated Title

--- header.php
+++ header.php
@@ -9,7 +9,6 @@
     <!-- AMP Style  -->
     <style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
 
-    <title><?php echo wp_title("-", true, 'right') ?></title>
     <?php wp_head() ?>
   </head>
   <body>
