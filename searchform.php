239494e78059db0f7db3eeecf5dfca6c6e32ef62
Arnon Puitrakul <peter_arnon1@icloud.com>
2017-08-07 00:03:41 +0700

    Reengineer search box to use css flexbox

--- searchform.php
+++ searchform.php
@@ -1,8 +1,8 @@
 <div class = "searchbox-outer">
   <form role = "search" method = "get" action =  "<?php echo home_url('/')?>">
-    <div class="input-field">
-      <i class = "fa fa-search search-font-color prefix" style = "margin-top:8px"></i>
-      <input type = "text" aria-label="search-box" name = "s" placeholder="Search something new!">
+    <div class="searchbox-container">
+      <i class = "fa fa-search fa-2x search-icon"></i>
+      <input class = "search-input" type = "text" aria-label="search-box" name = "s" placeholder="Search something new!">
     </div>
   </form>
 </div>
